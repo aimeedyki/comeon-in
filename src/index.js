9d1afde6657c0ec31f2fa5ca108d0bb7cb10fa4e
aimeedyki <akuwudike.a@gmail.com>
2020-03-12 00:18:00 +0100

    set up app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () =>
+  <div className="app">
+    <h1>hello app</h1>
+  </div>;
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
